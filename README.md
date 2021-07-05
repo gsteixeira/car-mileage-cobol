40acc4ed392844ed4d99ff6ed6e76a4808135df9
Gustavo S. Teixeira <gsteixei@gmail.com>
2021-07-05 02:24:17 -0300

    added environment impact calculations

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # car-mileage-cobol
 Calculate car mileage from csv file in COBOL.
+Also shows CO2 released, CO2 per km, and how many trees needs to plant.
 
-You have supply a csv file as the input in the following format:
+You must supply a csv file as the input in the following format:
 
 > odometer_read;gas_quantity;is_top_up?
 
@@ -9,20 +10,48 @@ You have supply a csv file as the input in the following format:
 
 - compiles like this:
 
-> cobc -x --free mileage.cob
+> cobc -x --free carmileage.cob
 
 - runs like this:
 
-> ./mileage sample.csv
+> ./carmileage sample.csv
 
-Why in COBOL?
+
+- Why in COBOL?
 
 Because it's so fun! :)
 
-My car is spending too much gas!
+
+- My car is spending too much gas!
 
 Check the pointers, tune them up. Clean the carburetors, tune them up. Tune the valves. Check spark plugs and cables. Use everything in standard setting: gicleurs, injectors, openings. Please refer to the user manual. A clean, well tuned carburetor and pointer runs better than any electronic garbage! 8)
 
-But my car is fuel injected!
 
-Ok, you ride a plastic toy!! Sell it, buy an electric or a classic! Carbs runs better.
+- But my car is fuel injected..
+
+Ok, so you ride a plastic toy... Sell it, buy an electric or a classic! Carbs runs better.
+
+
+- Carburetors and pointers?! This is something from the past..
+
+So why are you messing around with COBOL?
+
+
+- But fossil fuels are bad!!!
+
+You are indeed RIGHT! And that's why I made this software. Now you know how much you spend on fuel. By the way... COBOL is so good in doing calculations, and so fun, I also added calculations on CO2 released to the atmosphere. So now there is a column for CO2 released and another for CO2 per km.
+
+
+- The CO2 data makes me feel bad, what can I do to help?
+
+If viable, buy an electric vehicle. If you have a classic like I do, consider converting it to electric as soon as possible. But while it's not viable for most of us, the best you can do now is to **PLANT TREES**! Plant as many as you can. By the way, COBOL is so fun, I also added calculations on how many trees you should plant to compensate for all this released CO2.
+
+
+- How is the environmental impact calculated?
+
+2.31 Kg of CO2 per Liter of Gasoline burned.
+0.060 metric ton CO2 per urban tree planted.
+source: https://www.epa.gov/energy/greenhouse-gases-equivalencies-calculator-calculations-and-references
+
+
+Plant trees and have fun!
--- carmileage.cob
+++ carmileage.cob
@@ -26,7 +26,7 @@
         PROGRAM-ID. car-mileage.
             AUTHOR. gsteixei@gmail.com
             DATE-WRITTEN. 2021-07-04
-            INSTALLATION. cobc -x --free mileage.cob
+            INSTALLATION. cobc -x --free carmileage.cob
         ENVIRONMENT DIVISION.
             INPUT-OUTPUT SECTION.
                 FILE-CONTROL.
@@ -37,6 +37,9 @@
                 FD  csv_file.
                     01 line_record  PIC X(120) VALUE SPACES.
             WORKING-STORAGE SECTION.
+                01 CONST_GAS_CO2_KG    PIC 9V99 VALUE 2.31.
+                01 CONST_DIESEL_CO2_KG PIC 9V99 VALUE 2.73.
+                01 CONST_TREE_TON_CO2  PIC 9V99 VALUE 0.06.
                 01 csv_data.
                     02 csv_file_name   PIC X(125) VALUE SPACES.
                     01 end_of_file     PIC 9 VALUE ZERO.
@@ -48,6 +51,14 @@
                         03 dist        PIC 9999 VALUE ZEROS.
                         03 spent       PIC 999V99 VALUE ZEROS.
                         03 mileage     PIC 99V99 VALUE ZEROS.
+                    02 environmental_impact.
+                        03 kg_co2      PIC 999V99 VALUE ZEROS.
+                        03 co2_by_km   PIC 9V999 VALUE ZEROS.
+                        03 trees       PIC 99V99 VALUE ZEROS.
+                01 enviromental_stats.
+                    02 total_co2_kg    PIC 9999V99 VALUE ZEROS.
+                    02 total_co2_tons  PIC 99V999 VALUE ZEROS.
+                    02 total_trees     PIC 999V99 VALUE ZEROS.
                 01 aux_vars.
                     02 i               PIC 99 VALUE ZEROS.
                     02 j               PIC 99 VALUE ZEROS.
@@ -56,6 +67,10 @@
                     02 distance        PIC 9999 VALUE ZEROS.
                     02 fuel_spent      PIC 999V99 VALUE ZEROS.
                     02 last_odo        PIC 999999 VALUE ZEROS.
+                    02 total_mileage   PIC 9999V99 VALUE ZEROS.
+                    02 total_fuel      PIC 9999V99 VALUE ZEROS.
+                    02 count_topups    PIC 99 VALUE ZEROS.
+                    02 avg_mileage     PIC 99V9 VALUE ZEROS.
         PROCEDURE DIVISION.
             main.
                 ACCEPT csv_file_name FROM COMMAND-LINE.
@@ -73,8 +88,8 @@
             STOP RUN.
             
             compute_mileage.
-                PERFORM VARYING i FROM 2 BY 1 UNTIL
-                        odometer (i) = 0 OR i >= max_records
+                PERFORM VARYING i FROM 2 BY 1 UNTIL odometer (i) = 0 
+                                                    OR i >= max_records
                     MOVE fuel (i) TO fuel_spent
                     IF topup (i - 1) = 1 THEN
                         MOVE odometer (i - 1) TO last_odo
@@ -92,23 +107,43 @@
                     DIVIDE distance BY fuel_spent GIVING mileage (i)
                     MOVE distance TO dist (i)
                     MOVE fuel_spent TO spent (i)
+                    *> compute enviromental impact
+                    COMPUTE kg_co2 (i) = fuel_spent * CONST_GAS_CO2_KG
+                    COMPUTE co2_by_km (i) = kg_co2 (i) / distance
+                    COMPUTE trees (i) = (kg_co2 (i) / 1000) / CONST_TREE_TON_CO2
+                    IF topup (i) = 1 THEN
+                        ADD mileage (i) TO total_mileage
+                        ADD 1 TO count_topups
+                    END-IF
+                    ADD fuel (i) TO total_fuel
                     *>DISPLAY "d " distance " f " fuel_spent " m " mileage (i)
+                    *>DISPLAY "e " kg_co2 (i) " c " tones " m " trees (i)
                 END-PERFORM.
 
             show_results.
-                DISPLAY "id odometer fill  topup dist. fuel mileage"
-                PERFORM VARYING i from 1 BY 1 UNTIL
-                        odometer (i) = 0 OR i >= max_records
+                DISPLAY "id odometer fill  kgCO2  trees kgco2/km mileage  "
+                PERFORM VARYING i from 1 BY 1 UNTIL odometer (i) = 0 
+                                                    OR i >= max_records
                     DISPLAY i SPACE
-                            odometer (i) SPACE SPACE SPACE SPACE
-                            fuel (i) SPACE
-                            topup (i) SPACE SPACE SPACE SPACE SPACE
-                            dist (i) SPACE SPACE 
-                            spent (i) SPACE
-                            mileage (i) SPACE
+                            odometer (i) SPACE
+                            fuel (i) SPACE SPACE
+                            *>topup (i) SPACE SPACE SPACE SPACE
+                            *>dist (i) SPACE SPACE 
+                            *>spent (i) SPACE 
+                            kg_co2 (i) SPACE
+                            trees (i) SPACE
+                            co2_by_km (i) SPACE SPACE SPACE SPACE
+                            mileage (i) SPACE SPACE
                     END-DISPLAY
                 END-PERFORM.
-            
+                MULTIPLY total_fuel BY CONST_GAS_CO2_KG GIVING total_co2_kg.
+                DIVIDE total_co2_kg BY 1000 GIVING total_co2_tons.
+                DISPLAY "Total Tons of CO2: " total_co2_tons.
+                COMPUTE total_trees = total_co2_tons / CONST_TREE_TON_CO2
+                DISPLAY "Need to plant: " total_trees " trees!".
+                COMPUTE avg_mileage = total_mileage / count_topups
+                DISPLAY "Average mileage: " avg_mileage.
+
             load_from_csv.
                 DISPLAY "Loading data from CSV..."
                 MOVE 1 TO i.
@@ -128,5 +163,4 @@
                     ADD 1 TO i
                 END-PERFORM
                 CLOSE csv_file.
-                
-END PROGRAM car-mileage.
+        END PROGRAM car-mileage.
